80f9467dd0836f2cf1e93910040f5be39f865a0a
Ariel Talbot <Ariel.Talbot@iconfitness.com>
2021-05-17 23:51:34 -0600

    adding basic gradients and template pages

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -2,11 +2,11 @@ import React from 'react'
 import { graphql } from 'gatsby'
 import get from 'lodash/get'
 import { Helmet } from 'react-helmet'
-import styles from './blog.module.css'
+import styles from './contact.module.css'
 import Layout from '../components/layout'
 import ArticlePreview from '../components/article-preview'
 
-class BlogIndex extends React.Component {
+class ContactIndex extends React.Component {
   render() {
     const siteTitle = get(this, 'props.data.site.siteMetadata.title')
     const posts = get(this, 'props.data.allContentfulBlogPost.edges')
@@ -15,29 +15,17 @@ class BlogIndex extends React.Component {
       <Layout location={this.props.location}>
         <div style={{ background: '#fff' }}>
           <Helmet title={siteTitle} />
-          <div className={styles.hero}>Blog</div>
-          <div className="wrapper">
-            <h2 className="section-headline">Projects and Posts</h2>
-            <ul className="article-list">
-              {posts.map(({ node }) => {
-                return (
-                  <li key={node.slug}>
-                    <ArticlePreview article={node} />
-                  </li>
-                )
-              })}
-            </ul>
-          </div>
+          <div className={styles.hero}>Contact Me</div>
         </div>
       </Layout>
     )
   }
 }
 
-export default BlogIndex
+export default ContactIndex
 
 export const pageQuery = graphql`
-  query BlogIndexQuery {
+  query ContactIndexQuery {
     allContentfulBlogPost(sort: { fields: [publishDate], order: DESC }) {
       edges {
         node {
--- src/pages/resume.js
+++ src/pages/resume.js
@@ -2,11 +2,11 @@ import React from 'react'
 import { graphql } from 'gatsby'
 import get from 'lodash/get'
 import { Helmet } from 'react-helmet'
-import styles from './blog.module.css'
+import styles from './resume.module.css'
 import Layout from '../components/layout'
 import ArticlePreview from '../components/article-preview'
 
-class BlogIndex extends React.Component {
+class ResumeIndex extends React.Component {
   render() {
     const siteTitle = get(this, 'props.data.site.siteMetadata.title')
     const posts = get(this, 'props.data.allContentfulBlogPost.edges')
@@ -15,29 +15,17 @@ class BlogIndex extends React.Component {
       <Layout location={this.props.location}>
         <div style={{ background: '#fff' }}>
           <Helmet title={siteTitle} />
-          <div className={styles.hero}>Blog</div>
-          <div className="wrapper">
-            <h2 className="section-headline">Projects and Posts</h2>
-            <ul className="article-list">
-              {posts.map(({ node }) => {
-                return (
-                  <li key={node.slug}>
-                    <ArticlePreview article={node} />
-                  </li>
-                )
-              })}
-            </ul>
-          </div>
+          <div className={styles.hero}>Resume</div>
         </div>
       </Layout>
     )
   }
 }
 
-export default BlogIndex
+export default ResumeIndex
 
 export const pageQuery = graphql`
-  query BlogIndexQuery {
+  query ResumeIndexQuery {
     allContentfulBlogPost(sort: { fields: [publishDate], order: DESC }) {
       edges {
         node {
