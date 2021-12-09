66f652a741feb8b788937bb5b99162f936534a5e
Ariel Adams <ariel.talbot@iconfitness.com>
2021-12-09 13:37:27 -0700

    simplify menu

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -11,12 +11,6 @@ export default () => (
       <li className={styles.navigationItem}>
         <Link to="/blog/">Blog</Link>
       </li>
-      <li className={styles.navigationItem}>
-        <Link to="/resume/">Resume</Link>
-      </li>
-      <li className={styles.navigationItem}>
-        <Link to="/contact/">Contact</Link>
-      </li>
     </ul>
   </nav>
 )
